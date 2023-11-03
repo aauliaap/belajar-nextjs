50476aabde52cccef6e4f449b571b2b1221e5aca
aaulliaap <aaulliaap12@gmail.com>
2023-11-03 11:09:53 +0700

    commit

---
+++ pages/contact.js
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar';
+
+const contact = () => {
+    return (
+        <>
+            <Navbar />
+            <h1>Ini halaman contact</h1>
+        </>
+    );
+};
+
+export default contact;
---
+++ pages/portofolio.js
@@ -0,0 +1,12 @@
+import Navbar from './components/Navbar';
+
+const portofolio = () => {
+    return (
+        <>
+            <Navbar />
+            <h1>Ini halaman portofolio</h1>
+        </>
+    );
+};
+
+export default portofolio;
